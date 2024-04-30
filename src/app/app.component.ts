b896c8e667f0d5978bfb40a8f27e23a7281779e8
AlaMdalla <101179732+AlaMdalla@users.noreply.github.com>
2024-04-30 12:55:43 +0100

    add history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { TimelineComponent } from "./timeline/timeline.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent]
+    imports: [RouterOutlet, HeaderComponent, HomeComponent, AboutComponent, TimelineComponent]
 })
 export class AppComponent {
   title = 'Fottball';
